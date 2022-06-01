dc3316fa4111432eeeffdf99a29d3718a393a5b7
khalil0525 <collinskhalil@hotmail.com>
2022-06-01 15:17:14 -0400

    initial changes to db.js file

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,8 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost:5432/messenger",
-  {
-    logging: false,
-  }
-);
+const db = new Sequelize("messenger", "postgres", "gggabc123", {
+  host: "localhost",
+  dialect: "postgres",
+});
 
 module.exports = db;
