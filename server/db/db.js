da243541184dc22aac25f1d02ffa3a9564e7d264
khalil0525 <collinskhalil@hotmail.com>
2022-06-02 09:11:10 -0400

    fixed testing bug in database file

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,13 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("messenger", "postgres", "gggabc123", {
-  host: "localhost",
-  dialect: "postgres",
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || "messenger",
+  "postgres",
+  "gggabc123",
+  {
+    host: "localhost",
+    dialect: "postgres",
+  }
+);
 
 module.exports = db;
