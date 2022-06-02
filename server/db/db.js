594ccf3188b3779027eb184bbd86bc968053accb
khalil0525 <collinskhalil@hotmail.com>
2022-06-02 09:28:34 -0400

    added dialect variable to parameter in db file

--- server/db/db.js
+++ server/db/db.js
@@ -4,6 +4,7 @@ const db = new Sequelize(
   process.env.DATABASE_URL || "postgres://localhost:5432/messenger",
   {
     logging: false,
+    dialect: "postgres",
   } || "messenger",
   "postgres",
   "gggabc123",
