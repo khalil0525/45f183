8fdfa35d6eb7f885be1fc33dcdce3be54571ea62
khalil0525 <collinskhalil@hotmail.com>
2022-06-02 20:53:18 -0400

    added isRead to message insert

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -13,7 +13,12 @@ router.post("/", async (req, res, next) => {
 
     // if we already know conversation id, we can save time and just add it to message and return
     if (conversationId) {
-      const message = await Message.create({ senderId, text, conversationId });
+      const message = await Message.create({
+        senderId,
+        text,
+        conversationId,
+        isRead: 0,
+      });
       return res.json({ message, sender });
     }
     // if we don't have conversation id, find a conversation to make sure it doesn't already exist
@@ -36,6 +41,7 @@ router.post("/", async (req, res, next) => {
       senderId,
       text,
       conversationId: conversation.id,
+      isRead: 0,
     });
     res.json({ message, sender });
   } catch (error) {
