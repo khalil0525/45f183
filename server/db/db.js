1647be08adb85ff7b6cc7744ac0e5080996d1ee4
khalil0525 <collinskhalil@hotmail.com>
2022-06-03 19:40:48 -0400

    removed db parameters from db file

--- server/db/db.js
+++ server/db/db.js
@@ -1,18 +1,10 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("messenger", "postgres", "gggabc123", {
-  host: "localhost",
-  dialect: "postgres",
-});
-//PARAMETERS FOR GITHUB PULL REQUEST
-// process.env.DATABASE_URL || "postgres://localhost:5432/messenger",
-// {
-//   logging: false,
-// }
-//PARAMETERS FOR LOCAL MACHINE
-// "messenger", "postgres", "gggabc123", {
-//   host: "localhost",
-//   dialect: "postgres",
-// }
+const db = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost:5432/messenger",
+  {
+    logging: false,
+  }
+);
 
 module.exports = db;
