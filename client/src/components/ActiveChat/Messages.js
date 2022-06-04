2b5512461645905abafdcb3cc57cd3fded73318f
khalil0525 <collinskhalil@hotmail.com>
2022-06-03 22:24:31 -0400

    created function to get the last message recipient read

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from ".";
 import moment from "moment";
@@ -6,13 +6,29 @@ import moment from "moment";
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
+  const getLastReadMessageId = useMemo(() => {
+    for (let index = messages.length - 1; index >= 0; index--) {
+      if (messages[index].isRead && messages[index].senderId === userId) {
+        return index;
+      }
+    }
+    return 0;
+  }, [messages, userId]);
+
+  console.log(messages);
   return (
     <Box>
       {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <SenderBubble
+            key={message.id}
+            text={message.text}
+            time={time}
+            otherUser={otherUser}
+            lastReadMessageId={getLastReadMessageId}
+          />
         ) : (
           <OtherUserBubble
             key={message.id}
