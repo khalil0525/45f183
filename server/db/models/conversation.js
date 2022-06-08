b28dc8ce31d38772b302dbc9b62bc2876d64377e
khalil0525 <collinskhalil@hotmail.com>
2022-06-07 20:13:12 -0400

    changed the where clause

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,23 +1,18 @@
-const { Op } = require("sequelize");
 const db = require("../db");
 const Message = require("./message");
+const Sequelize = require("sequelize");
 
 const Conversation = db.define("conversation", {});
 
 // find conversation given two user Ids
 
-Conversation.findConversation = async function (user1Id, user2Id) {
+Conversation.findConversation = async function (userId, convoId) {
   const conversation = await Conversation.findOne({
     where: {
-      user1Id: {
-        [Op.or]: [user1Id, user2Id],
-      },
-      user2Id: {
-        [Op.or]: [user1Id, user2Id],
-      },
+      userId,
+      conversationId: convoId,
     },
   });
-
   // return conversation or null if it doesn't exist
   return conversation;
 };
